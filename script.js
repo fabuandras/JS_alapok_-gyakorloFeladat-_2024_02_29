65204bbe855662df6e087fb35b88033bd9fcb849
Fabu András <fabuandras@gmail.com>
2024-02-29 12:30:33 +0100

    JS_alapok_[gyakorloFeladat]_2024_02_29

---
+++ script.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", function () {
+    elemekElerese1();
+    elemekElerese2();
+    elemekElerese3();
+    elemekElerese4();
+    elemekElerese5();
+    elemekElerese6();
+    elemekElerese7();
+    elemekElerese8();
+    elemekElerese9();
+    elemekElerese10();
+});
+
+function elemekElerese1() {
+    /* 1. feladat */
+    const ELEM=document.querySelector("#f1")
+    console.log(ELEM.innerHTML)
+}
+function elemekElerese2() {
+    /* 2. feladat */
+    const ELEM=document.querySelector("#ide")
+    console.log(ELEM)
+    ELEM.innerHTML = "<p>Jó reggelt! </p>"
+}
+function elemekElerese3() {
+    /* 3. feladat */
+    const ELEM=document.querySelector(".ide")
+    console.log(ELEM)
+    ELEM.innerHTML = "<p>Jó reggelt! </p>"
+}
+function elemekElerese4() {
+    /* 4. feladat */
+    ELEM=document.querySelector("")
+}
